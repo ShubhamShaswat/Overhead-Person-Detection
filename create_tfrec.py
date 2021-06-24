5d108a5cd430d4b2673c7962fb553f56daf1adf3
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-06-24 21:51:38 +0530

    added view tfrec

--- create_tfrec.py
+++ create_tfrec.py
@@ -92,3 +92,48 @@ def create_tfrecord(file_dir,name='gotpd-training'):
             writer.write(example)
 
 create_tfrecord(file_dir=file_dir)
+
+#-----------------------------------------
+
+IMAGE_SIZE = [128,128]
+#AUTO = tf.data.experimental.AUTOTUNE
+
+# Create a dictionary describing the features.
+image_feature_description = {
+
+    'image_input': tf.io.FixedLenFeature([], tf.string),
+    'image_output': tf.io.FixedLenFeature([], tf.string),
+}
+
+def _parse_image_function(example_proto):
+  # Parse the input tf.Example proto using the dictionary above.
+  parsed_example = tf.io.parse_single_example(example_proto, image_feature_description)
+  image = decode_image(parsed_example['image_input'])
+  return image
+
+def decode_image(image_data):
+    #decode image 
+    image = tf.image.decode_jpeg(image_data, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0  # convert image to floats in [0, 1] range
+    image = tf.image.resize(image,IMAGE_SIZE)
+    return image
+
+ #function to plot images
+def view_image(ds):
+    images = next(iter(ds)) # extract 1 batch from the dataset
+    images = images.numpy()
+
+    fig = plt.figure(figsize=(20, 20))
+    for i in range(20):
+        ax = fig.add_subplot(4, 5, i+1, xticks=[], yticks=[])
+        ax.imshow(images[i])
+
+def get_dataset(data_dir):
+    filenames = tf.io.gfile.glob(data_dir)
+    dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
+    dataset = dataset.map(_parse_image_function, num_parallel_calls=AUTO)
+    dataset = dataset.batch(BATCH_SIZE)
+    return dataset
+
+ds = get_dataset('./gotpd-training.tfrecord')
+view_image(ds)
