07d54686aa4e7455b7e4d2406ed56976ee713b62
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-06-25 21:00:10 +0530

    encooder model initial commit

--- models/encoder.py
+++ models/encoder.py
@@ -1,1 +1,15 @@
 #encoder module
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import (
+    Dense, Dropout, Activation, Flatten, Input, BatchNormalization, UpSampling2D, Add, Conv2D, MaxPooling2D, LeakyReLU
+)
+
+
+##encoder 
+
+def encoder():
+
+	inp = Input(shape=shape)
+	x = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same')(inp)
