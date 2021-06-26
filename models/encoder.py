e521d7da32a32a9f97117cb2c060132effec03e9
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-06-26 23:36:44 +0530

    encodingg block test

--- models/encoder.py
+++ models/encoder.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import (
 
 #Encoding Conv Block
 def encoding_block(x,a,b,c,k):
-	y = Conv2D(a, kernel_size=(1, 1), strides=(2, 2), padding='same')(y)
+	y = Conv2D(a, kernel_size=(1, 1), strides=(2, 2), padding='same')(x)
 	y = BatchNormalization()(y)
 	y = LeakyReLU()(y)
 	y = Conv2D(b, kernel_size=(k, k), strides=(1, 1), padding='same')(y)
@@ -34,6 +34,13 @@ def encoder(shape=(212,256,1)):
 	x = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(inp)
 	x = BatchNormalization()(x)
 	x = LeakyReLU()(x)
+	x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x) #doubt about strides value
+
+	x = encoding_block(x,a=64,b=64,c=256,k=3)
+	x = encoding_block(x,a=128,b=128,c=512,k=3)
+	x = encoding_block(x,a=256,b=256,c=1024,k=3)
+
+
 
 
 
