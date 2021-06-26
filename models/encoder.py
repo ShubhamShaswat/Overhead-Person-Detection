ab56a07995e7701868e435fabf3bd6d77cc88a40
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-06-26 23:30:37 +0530

    encodingg block added

--- models/encoder.py
+++ models/encoder.py
@@ -3,8 +3,28 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.layers import (
-    Dense, Dropout, Activation, Flatten, Input, BatchNormalization, UpSampling2D, Add, Conv2D, MaxPooling2D, LeakyReLU
+    Dense, Dropout, Activation, Flatten, Input, BatchNormalization, UpSampling2D, Add, Conv2D, MaxPooling2D, LeakyReLU, Add
 )
+
+#Encoding Conv Block
+def encoding_block(x,a,b,c,k):
+	y = Conv2D(a, kernel_size=(1, 1), strides=(2, 2), padding='same')(y)
+	y = BatchNormalization()(y)
+	y = LeakyReLU()(y)
+	y = Conv2D(b, kernel_size=(k, k), strides=(1, 1), padding='same')(y)
+	y = BatchNormalization()(y)
+	y = LeakyReLU()(y)
+	y = Conv2D(c, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+	y = BatchNormalization()(y)
+	y = LeakyReLU()(y)
+	
+	y_shortcut = Conv2D(c, kernel_size=(1 1), strides=(2, 2), padding='same')(x)
+	y_shortcut = BatchNormalization()(y_shortcut)
+	y_out = Add()([y_shortcut,y])
+	y_out = LeakyReLU(y_out)
+
+	return y_out
+
 
 ###Main Block
 #encoder 
@@ -14,18 +34,6 @@ def encoder(shape=(212,256,1)):
 	x = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(inp)
 	x = BatchNormalization()(x)
 	x = LeakyReLU()(x)
-
-	#Encoding Conv Block
-	x = Conv2D(64, kernel_size=(1, 1), strides=(1, 1), padding='same')(x)
-	x = BatchNormalization()(x)
-	x = LeakyReLU()(x)
-	x = Conv2D(64, kernel_size=(1, 1), strides=(2, 2), padding='same')(x)
-	x = BatchNormalization()(x)
-	x = LeakyReLU()(x)x = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
-	x = BatchNormalization()(x)
-	x = LeakyReLU()(x)
-
-	
 
 
 
