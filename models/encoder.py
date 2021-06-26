5e286ef7d6b90a090077797a351053d15184bd6e
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-06-26 23:19:46 +0530

    progreesing on main block model

--- models/encoder.py
+++ models/encoder.py
@@ -6,10 +6,26 @@ from tensorflow.keras.layers import (
     Dense, Dropout, Activation, Flatten, Input, BatchNormalization, UpSampling2D, Add, Conv2D, MaxPooling2D, LeakyReLU
 )
 
-
-##encoder 
-
-def encoder():
+###Main Block
+#encoder 
+def encoder(shape=(212,256,1)):
 
 	inp = Input(shape=shape)
-	x = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same')(inp)
+	x = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(inp)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+
+	#Encoding Conv Block
+	x = Conv2D(64, kernel_size=(1, 1), strides=(1, 1), padding='same')(x)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	x = Conv2D(64, kernel_size=(1, 1), strides=(2, 2), padding='same')(x)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)x = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+
+	
+
+
+
