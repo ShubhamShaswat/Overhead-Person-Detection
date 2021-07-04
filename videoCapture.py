b291b99f437ef6865637b4260cf71ec15fce63a9
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-07-04 23:25:25 +0530

    plt people and tarck

--- videoCapture.py
+++ videoCapture.py
@@ -4,22 +4,60 @@ import matplotlib.pyplot as plt
 import os
 from PIL import Image
 import numpy as np
-directory = '/home/shubham/Downloads/gotpd_test-inputs/seq-P05-M04-A0001-G03-C00-S0030'
+
+out_directory = '/home/shubham/Downloads/gotpd_test_outputs/seq-P05-M04-A0001-G03-C00-S0030'
+inp_directory = '/home/shubham/Downloads/gotpd_test_inputs/seq-P05-M04-A0001-G03-C00-S0030'
+
+inp_images = os.listdir(inp_directory)
+out_images = os.listdir(out_directory)
+
+inp_images.sort()
+out_images.sort()
+
+index = len(inp_images) // 2
+
+img = Image.open(os.path.join(inp_directory,inp_images[index]))
+labels = Image.open(os.path.join(out_directory,out_images[index]))
+
+img = np.asarray(img)
+labels = np.asarray(labels)
+
+im = np.add(img,labels)
+
+#z = np.zeros((424, 512, 1)) 
+#g = im[:,:,1]
+#g = np.expand_dims(g,axis=-1)
+#new_im = np.concatenate((z,g,z),axis=-1)
+#np.place(r, r == 255, 0.)
+#max_pixel, min_pixel = np.max(im),np.min(im)
+#print(max_pixel,min_pixel)
+#plt.imshow(im)
+#plt.show()
+
+
+
+
 
 fig = plt.figure( 1 )
 ax = fig.add_subplot( 111 )
 ax.set_title("My Title")
 
-im = ax.imshow( numpy.zeros( ( 512, 424, 3 ) ) ) # Blank starting image
+im = ax.imshow( numpy.zeros( ( 424, 512, 3 ) ) ) # Blank starting image
 fig.show()
 im.axes.figure.canvas.draw()
 
 tstart = time.time()
-for a in os.listdir(directory):
-  data = Image.open(directory + '/' + a)
-  data = np.asarray(data)
-                                         #numpy.random.random( ( 256, 256, 3 ) ) # Random image to display
-  ax.set_title( str( a ) )
+for i in range(len(inp_images)) :
+
+  img = Image.open(os.path.join(inp_directory,inp_images[i]))
+  labels = Image.open(os.path.join(out_directory,out_images[i]))
+
+  img = np.asarray(img)
+  labels = np.asarray(labels)
+
+  data = np.add(img,labels)
+
+  #ax.set_title( str( a ) )
   im.set_data( data )
   im.axes.figure.canvas.draw()
 
