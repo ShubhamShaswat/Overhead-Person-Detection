5b405e372aad5748c3390a706eb29423c7382764
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-07-09 23:11:15 +0530

    tfx pipleine

---
+++ tfx_pipeline.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+print('TensorFlow version: {}'.format(tf.__version__))
+from tfx import v1 as tfx
+print('TFX version: {}'.format(tfx.__version__))
+
+
+
+
+##-------------------------------------------------------------TFX PIPELINE-----------------------------------------------
+def _create_pipeline(pipeline_name: str, pipeline_root: str, data_root: str,
+                     module_file: str, serving_model_dir: str,
+                     metadata_path: str) -> tfx.dsl.Pipeline:
+  """Creates a three component penguin pipeline with TFX."""
+  # Brings data into the pipeline.
+  example_gen = tfx.components.CsvExampleGen(input_base=data_root)
+
+  # Uses user-provided Python function that trains a model.
+  trainer = tfx.components.Trainer(
+      module_file=module_file,
+      examples=example_gen.outputs['examples'],
+      train_args=tfx.proto.TrainArgs(num_steps=100),
+      eval_args=tfx.proto.EvalArgs(num_steps=5))
+
+  # Pushes the model to a filesystem destination.
+  pusher = tfx.components.Pusher(
+      model=trainer.outputs['model'],
+      push_destination=tfx.proto.PushDestination(
+          filesystem=tfx.proto.PushDestination.Filesystem(
+              base_directory=serving_model_dir)))
+
+  # Following three components will be included in the pipeline.
+  components = [
+      example_gen,
+      trainer,
+      pusher,
+  ]
+
+  return tfx.dsl.Pipeline(
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
+      metadata_connection_config=tfx.orchestration.metadata
+      .sqlite_metadata_connection_config(metadata_path),
+      components=components)
